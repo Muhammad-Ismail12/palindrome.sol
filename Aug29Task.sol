3cda848843d741e5e5e320ab7370ead899bee2d5
Muhammad Ismail <110536675+Muhammad-Ismail12@users.noreply.github.com>
2022-08-31 00:18:42 +0500

    Create Aug29Task.sol

---
+++ Aug29Task.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity  <0.9.0;
+
+contract Task1{
+
+    function power(uint x , uint y) public pure returns(uint){
+       return x ** y;
+    }
+}
+
+contract Task2{
+
+    function palindrome(uint n) public pure returns(uint){
+        uint result = 0;
+        uint remainder;
+        uint n_OriginalValue;
+        n_OriginalValue = n;
+        
+        while (n > 0){
+            remainder = n % 10;
+            result = result * 10 + remainder;
+            n = n/10;
+        }
+        if (n_OriginalValue == result){
+            return 1; //palindrome
+        }
+        else{
+            return 0; // not palindrome
+        }
+    }
+}
