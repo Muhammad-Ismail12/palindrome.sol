b83b829daa3169664cce861292a86f650731d43b
Muhammad Ismail <110536675+Muhammad-Ismail12@users.noreply.github.com>
2022-09-18 07:25:02 -0700

    Create X raised Y.sol

---
+++ X raised Y.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity  <0.9.0;
+
+contract Task1{
+
+    function power(uint x , uint y) public pure returns(uint){
+       return x ** y;
+    }
+}
